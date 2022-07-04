8a3a244e2116a394635357fa4ac19ae13801d084
dnksebastian <sebastian.dulniawka@gmail.com>
2022-07-04 23:48:47 +0200

    js calculator

--- tip-calculator-app-main/app.js
+++ tip-calculator-app-main/app.js
@@ -7,6 +7,9 @@ const radioElementsArray = [...radioElements];
 
 const peopleNumInputValue = document.getElementById("peoplenumber");
 
+const allInputElements = document.querySelectorAll('input[type="number"]');
+const arrInputElements = Array.from(allInputElements);
+
 const tipPersonValue = document.getElementById("tip-person");
 const tipTotalValue = document.getElementById("tip-total");
 
@@ -18,46 +21,93 @@ let amountTotalPerPerson;
 /* -=-=-=-=-=-=-=-=-=-=-=-=- */
 
 function calculateTipValues() {
-    const radioTipElements = document.querySelector(
-      'input[name="radio-tip"]:checked'
-    );
-
   let billValue = parseFloat(billInputValue.value);
-  let tipValue = parseFloat(radioTipElements.value);
-  let customValue = parseFloat(radioTipCustom.value * 0.01);
   let peopleNumber = parseFloat(peopleNumInputValue.value);
-  
-  if (radioTipCustom.value !== "") {
-      radioElementsArray.forEach((element) => {
-          element.checked = false;
-        });
-        
-        let totalValue = billValue + billValue * customValue;
-        let totalPerPerson = totalValue / peopleNumber;
-        let tipPerPerson = (billValue * customValue) / peopleNumber;
 
-    } else {
-      let totalValue = billValue + billValue * tipValue;
-      let totalPerPerson = totalValue / peopleNumber;
-      let tipPerPerson = (billValue * tipValue) / peopleNumber;
-    }
+  let tipValue;
+
+  if (radioTipCustom.value !== "") {
+    tipValue = parseFloat(radioTipCustom.value * 0.01);
+  } else {
+    radioTipElement = document.querySelector('input[name="radio-tip"]:checked');
+    tipValue = parseFloat(radioTipElement.value);
+  }
   
-    
+  let totalValue = billValue + (billValue * tipValue);
+  let totalPerPerson = totalValue / peopleNumber;
+  let tipPerPerson = (billValue * tipValue) / peopleNumber;
+
   amountTipPerPerson = parseFloat(tipPerPerson.toFixed(2));
   amountTotalPerPerson = parseFloat(totalPerPerson.toFixed(2));
+
 }
 
-function validateCalculator() {}
+function buttonHandler() {
+  if (
+    billInputValue.value != "" ||
+    radioTipCustom.value != "" ||
+    peopleNumInputValue.value != "" ||
+    tipPersonValue.textContent != "$0.00" ||
+    tipTotalValue.textContent != "$0.00"
+  ) {
+    resetBtnElement.disabled = false;
+  } else {
+    resetBtnElement.disabled = true;
+  }
+}
+
+function calculatorValidation() {
+  if (peopleNumInputValue.value == 0) {
+    peopleNumInputValue.style.borderColor = "#B48372";
+    document.documentElement.style.setProperty(
+      "--afterelement-display",
+      "block"
+    );
+  } else {
+    peopleNumInputValue.style.borderColor = "transparent";
+    document.documentElement.style.setProperty(
+      "--afterelement-display",
+      "none"
+    );
+  }
+}
 
 function populateCalculatorFields() {
   tipPersonValue.textContent = `$${amountTipPerPerson}`;
   tipTotalValue.textContent = `$${amountTotalPerPerson}`;
 }
 
-peopleNumInputValue.addEventListener("keyup", calculateTipValues);
-peopleNumInputValue.addEventListener("keyup", populateCalculatorFields);
-billInputValue.addEventListener("keyup", validateCalculator);
-peopleNumInputValue.addEventListener("keyup", validateCalculator);
+function resetValues() {
+  arrInputElements.forEach((element) => {
+    element.value = "";
+  });
+  tipPersonValue.textContent = `$0.00`;
+  tipTotalValue.textContent = `$0.00`;
+}
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=- */
+
+peopleNumInputValue.addEventListener("keyup", calculateTipValues);
+peopleNumInputValue.addEventListener("keyup", populateCalculatorFields);
+
+radioElementsArray.forEach((e) => {
+  e.addEventListener("click", function () {
+    radioTipCustom.value = "";
+  });
+});
+
+radioTipCustom.addEventListener("click", function () {
+  radioElementsArray.forEach((element) => {
+    element.checked = false;
+  });
+});
+
+arrInputElements.forEach((element) => {
+  element.addEventListener("focus", buttonHandler);
+});
+
+peopleNumInputValue.addEventListener("focus", calculatorValidation);
+
+resetBtnElement.addEventListener("click", resetValues);
+
 /* -=-=-=-=-=-=-=-=-=-=-=-=- */
