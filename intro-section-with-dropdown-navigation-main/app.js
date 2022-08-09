40c1fbc40e19512d50c1d41750d8f5458e4e3069
dnksebastian <sebastian.dulniawka@gmail.com>
2022-08-09 23:19:17 +0200

    working on menu

---
+++ intro-section-with-dropdown-navigation-main/app.js
@@ -0,0 +1,14 @@
+const sideMenuOpenElement = document.getElementById("open-menu");
+const overlayElement = document.getElementById("side-overlay");
+const navigationElement = document.getElementById("nav-menu");
+
+
+
+
+
+
+
+
+
+
+console.log(sideMenuOpenElement, overlayElement, navigationElement);
