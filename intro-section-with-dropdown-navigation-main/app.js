19dee0cefebfcc02b6d6edd97521f992739de0f4
dnksebastian <sebastian.dulniawka@gmail.com>
2022-08-10 23:31:27 +0200

    styling

--- intro-section-with-dropdown-navigation-main/app.js
+++ intro-section-with-dropdown-navigation-main/app.js
@@ -1,14 +1,24 @@
 const sideMenuOpenElement = document.getElementById("open-menu");
+const sideMenuCloseElement = document.getElementById("close-menu");
 const overlayElement = document.getElementById("side-overlay");
 const navigationElement = document.getElementById("nav-menu");
 
 
 
+const closeMenu = () => {
+overlayElement.style.display = "none"
+navigationElement.style.display = "none"    
+};
+
+const openMenu = () => {
+overlayElement.style.display = "block"
+navigationElement.style.display = "flex"
+};
 
 
 
+sideMenuCloseElement.addEventListener("click", closeMenu);
+sideMenuOpenElement.addEventListener("click", openMenu);
+overlayElement.addEventListener("click", closeMenu);
 
-
-
-
-console.log(sideMenuOpenElement, overlayElement, navigationElement);
+console.log(sideMenuOpenElement, overlayElement, navigationElement, sideMenuCloseElement);
