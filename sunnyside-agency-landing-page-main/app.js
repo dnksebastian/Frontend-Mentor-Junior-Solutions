7ee881ea515957b3d84a96c0f708c07bcee379f0
dnksebastian <sebastian.dulniawka@gmail.com>
2022-09-28 01:06:19 +0200

    sunnyside styling mobile

--- sunnyside-agency-landing-page-main/app.js
+++ sunnyside-agency-landing-page-main/app.js
@@ -1,1 +1,17 @@
-console.log(".");
+/* Selecting elements */
+
+const hamburgerIconElement = document.getElementById('hamburger');
+
+const navigationElement = document.getElementById('navigation');
+
+/* Functions */
+
+const toggleMobileMenu = () => {
+    navigationElement.classList.toggle('showmenu');
+    hamburgerIconElement.classList.toggle('iconfade');
+};
+
+
+/* Event listeners */
+
+hamburgerIconElement.addEventListener('click', toggleMobileMenu);
