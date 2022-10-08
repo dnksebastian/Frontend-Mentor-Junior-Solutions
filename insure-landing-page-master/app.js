96ac8f33436d2e549579a907ec6cd0baefae66a7
dnksebastian <sebastian.dulniawka@gmail.com>
2022-10-09 00:00:35 +0200

    styling insure lp

--- insure-landing-page-master/app.js
+++ insure-landing-page-master/app.js
@@ -1,1 +1,15 @@
-console.log('.');
+// Elements
+
+const menuIconElement = document.getElementById('hamburger');
+const navbarTopElement = document.getElementById('navigation-top');
+
+// Functions
+
+const showNavbar = () => {
+    menuIconElement.classList.toggle('icon-toggle');
+    navbarTopElement.classList.toggle('show-nav');
+};
+
+// Event listeners
+
+menuIconElement.addEventListener('click', showNavbar);
