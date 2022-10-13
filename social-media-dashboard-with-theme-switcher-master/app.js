f11a056df3c32584238deff2fd9b03f2943119fb
dnksebastian <sebastian.dulniawka@gmail.com>
2022-10-14 00:00:48 +0200

    finished project sm dashboard

--- social-media-dashboard-with-theme-switcher-master/app.js
+++ social-media-dashboard-with-theme-switcher-master/app.js
@@ -1,1 +1,10 @@
-console.log('.');
+const rootElement = document.querySelector(":root");
+const toggleSwitchElement = document.getElementById("toggleSwitch");
+
+
+let toggleColorMode = () => {
+rootElement.classList.toggle('light');
+};
+
+
+toggleSwitchElement.addEventListener('click', toggleColorMode);
