1f7b30593215965a5f8cfb836863c97dd1089adc
dnksebastian <sebastian.dulniawka@gmail.com>
2022-10-30 21:06:28 +0100

    added toggle switch pricing component

--- pricing-component-with-toggle-master/app.js
+++ pricing-component-with-toggle-master/app.js
@@ -1,1 +1,29 @@
-console.log(".");
+// DOM elements
+const toggleCheckboxElement = document.getElementById('toggle-checkbox');
+
+const basicPriceValue = document.getElementById('basic-price');
+const professionalPriceValue = document.getElementById('professional-price');
+const masterPriceValue = document.getElementById('master-price');
+
+console.log(toggleCheckboxElement, basicPriceValue, professionalPriceValue, masterPriceValue);
+
+
+// Functions
+
+const togglePrices = () => {
+    if (toggleCheckboxElement.checked) {
+        basicPriceValue.textContent = "19.99";
+        professionalPriceValue.textContent = "24.99";
+        masterPriceValue.textContent = "39.99";;
+    }
+    else {
+        basicPriceValue.textContent = "199.99";
+        professionalPriceValue.textContent = "249.99";
+        masterPriceValue.textContent = "399.99"
+    };
+};
+
+
+
+// Listeners
+toggleCheckboxElement.addEventListener('click', togglePrices);
