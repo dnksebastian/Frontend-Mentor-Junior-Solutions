7055dbc401f0d9aa8edf5aa3d58ad5992a602fd2
dnksebastian <sebastian.dulniawka@gmail.com>
2022-11-08 00:54:36 +0100

    styling mobile news homepage menu

--- news-homepage-main/app.js
+++ news-homepage-main/app.js
@@ -1,1 +1,22 @@
-console.log('.');
+// DOM Elements
+const navElement = document.getElementById('navigation');
+const showMenuElement = document.getElementById('show-menu-icon');
+const closeMenuElement = document.getElementById('close-menu-icon');
+
+
+// Functions
+
+const showMenu = () => {
+navElement.classList.add('showmenu');
+};
+
+const closeMenu = () => {
+navElement.classList.remove('showmenu');
+};
+
+
+
+// Event listeners
+
+showMenuElement.addEventListener('click', showMenu);
+closeMenuElement.addEventListener('click', closeMenu);
