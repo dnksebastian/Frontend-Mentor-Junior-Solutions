59927f3251eda07230d6da08709e271b4e4be467
dnksebastian <sebastian.dulniawka@gmail.com>
2022-11-30 00:40:46 +0100

    styling mobile loopstudios lp

--- loopstudios-landing-page-main/main.js
+++ loopstudios-landing-page-main/main.js
@@ -1,1 +1,22 @@
-console.log(".");
+// DOM Elements
+
+const topNavEl = document.getElementById("top-nav");
+const topHeaderEl = document.getElementById("topheader");
+const hamburgerEl = document.getElementById("hamburger-wrapper");
+const openIconEl = document.getElementById("hamburger-open");
+const closeIconEl = document.getElementById("hamburger-close");
+
+console.log(topNavEl, topHeaderEl, hamburgerEl, openIconEl, closeIconEl);
+
+// Functions
+
+const toggleMobileMenu = () => {
+  topNavEl.classList.toggle("menu-active");
+  topHeaderEl.classList.toggle("menu-active");
+  openIconEl.classList.toggle("hide");
+  closeIconEl.classList.toggle("show");
+};
+
+// Event listeners
+
+hamburgerEl.addEventListener("click", toggleMobileMenu);
