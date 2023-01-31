73a8835901cd897d58b25ad2fccfb88f48fb506a
dnksebastian <sebastian.dulniawka@gmail.com>
2023-01-31 17:05:11 +0100

    blogr landing page

--- blogr-landing-page-main/main.js
+++ blogr-landing-page-main/main.js
@@ -1,1 +1,16 @@
-console.log('.');
+// DOM Elements
+const topMenuIconEl = document.getElementById("menu-icon");
+const topNavEl = document.getElementById("top-nav");
+
+console.log(topMenuIconEl, topNavEl);
+
+// Functions
+
+function toggleMenu() {
+  topNavEl.classList.toggle("visible");
+  topMenuIconEl.classList.toggle('menu-open');
+}
+
+// Listeners
+
+topMenuIconEl.addEventListener("click", toggleMenu);
