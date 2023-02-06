9405f7c7c605e9e11200fc8064de0c49be3ec086
dnksebastian <sebastian.dulniawka@gmail.com>
2023-02-06 22:16:00 +0100

    expenses chart component

--- expenses-chart-component-main/main.js
+++ expenses-chart-component-main/main.js
@@ -1,1 +1,50 @@
-console.log(".");
+// DOM Elements & global variables
+
+const chartElement = document.getElementById('graph');
+
+// Functions
+async function fetchData() {
+  try {
+    const chartData = await fetch("./data.json");
+    const dataArr = await chartData.json();
+    console.log(dataArr);
+    return dataArr;
+  } catch (error) {
+    return error;
+  }
+}
+
+function createLiEleement(day, amount) {
+
+    const liItem = document.createElement('li');
+    const valueElement = document.createElement('span');
+    const barElement = document.createElement('div');
+    const labelElement = document.createElement('span');
+    
+    liItem.classList.add('bar-wrap');
+    valueElement.classList.add('bar-value');
+    barElement.classList.add('bar');
+    labelElement.classList.add('bar-label');
+
+    liItem.appendChild(valueElement);
+    liItem.appendChild(barElement);
+    liItem.appendChild(labelElement);
+    
+    valueElement.textContent = amount;
+    labelElement.textContent = day;    
+    
+    chartElement.appendChild(liItem);
+}
+
+async function populateChart() {
+    const data = await fetchData();
+
+    data.forEach(el => {
+        const day = el.day;
+        const amount = el.amount;
+
+        createLiEleement(day, amount);
+    });
+};
+
+populateChart();
