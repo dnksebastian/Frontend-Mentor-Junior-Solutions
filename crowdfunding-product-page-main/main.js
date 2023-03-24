085021c9876d30dfaaeb71e1bb6e9e988ea5afc5
dnksebastian <sebastian.dulniawka@gmail.com>
2023-03-24 19:41:38 +0100

    crowdfunding product page

--- crowdfunding-product-page-main/main.js
+++ crowdfunding-product-page-main/main.js
@@ -1,1 +1,17 @@
-console.log(".");
+// DOM Elements & variables
+const mobMenuIconEl = document.getElementById('mobile-menu-control');
+const mobNavEl = document.getElementById('top-nav');
+const backdropEl = document.getElementById('backdrop');
+
+// Functions
+
+function toggleMenu() {
+    mobNavEl.classList.toggle('active');
+    mobMenuIconEl.classList.toggle('active');
+    backdropEl.classList.toggle('bdrop-active');
+}
+
+
+// Event listeners
+
+mobMenuIconEl.addEventListener('click', toggleMenu);
