ff0ddc0965c089ba49f04a8bbcfe30a75c8eb1c0
dnksebastian <sebastian.dulniawka@gmail.com>
2023-03-25 22:30:31 +0100

    crowdfunding product page

--- crowdfunding-product-page-main/main.js
+++ crowdfunding-product-page-main/main.js
@@ -3,6 +3,10 @@ const mobMenuIconEl = document.getElementById('mobile-menu-control');
 const mobNavEl = document.getElementById('top-nav');
 const backdropEl = document.getElementById('backdrop');
 
+const backProjectBtn = document.getElementById('back-project-btn');
+const closeModalBtn = document.getElementById('close-modal');
+const pledgesDialogEl = document.getElementById('selection-modal');
+
 // Functions
 
 function toggleMenu() {
@@ -11,7 +15,17 @@ function toggleMenu() {
     backdropEl.classList.toggle('bdrop-active');
 }
 
+function showModal() {
+pledgesDialogEl.showModal();
+}
+
+function closeModal() {
+pledgesDialogEl.close();
+};
+
 
 // Event listeners
 
 mobMenuIconEl.addEventListener('click', toggleMenu);
+backProjectBtn.addEventListener('click', showModal);
+closeModalBtn.addEventListener('click', closeModal);
