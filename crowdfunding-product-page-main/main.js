9474f70efd0626f4181fd9ad6d6e7c5e56db0b1d
dnksebastian <sebastian.dulniawka@gmail.com>
2023-03-27 23:06:17 +0200

    crowdfunding product page

--- crowdfunding-product-page-main/main.js
+++ crowdfunding-product-page-main/main.js
@@ -7,21 +7,40 @@ const backProjectBtn = document.getElementById('back-project-btn');
 const closeModalBtn = document.getElementById('close-modal');
 const pledgesDialogEl = document.getElementById('selection-modal');
 
+const modalOptionsElements = [... pledgesDialogEl.querySelectorAll('.pledge-li')];
+console.log(modalOptionsElements);
+
 // Functions
 
 function toggleMenu() {
     mobNavEl.classList.toggle('active');
     mobMenuIconEl.classList.toggle('active');
     backdropEl.classList.toggle('bdrop-active');
+    document.body.classList.toggle('body-no-scroll');
 }
 
 function showModal() {
 pledgesDialogEl.showModal();
+document.body.classList.add('body-no-scroll');
 }
 
 function closeModal() {
 pledgesDialogEl.close();
+document.body.classList.remove('body-no-scroll');
 };
+
+
+function pickedOption() {
+    modalOptionsElements.forEach((el) => {
+        if (el.querySelector('input').checked) {
+            console.log(el.querySelector('.pledge-name').textContent);
+            el.classList.add('option-picked');
+        } else {
+            el.classList.remove('option-picked');
+        }
+    });
+}
+
 
 
 // Event listeners
@@ -29,3 +48,6 @@ pledgesDialogEl.close();
 mobMenuIconEl.addEventListener('click', toggleMenu);
 backProjectBtn.addEventListener('click', showModal);
 closeModalBtn.addEventListener('click', closeModal);
+modalOptionsElements.forEach((el) => {
+    el.addEventListener('click', pickedOption);
+})
