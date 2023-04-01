9a68ea88ac1f27af8c6fd9fba6bafc0ea142572f
dnksebastian <sebastian.dulniawka@gmail.com>
2023-04-01 20:51:57 +0200

    age calculator app

--- age-calculator-app-main/main.js
+++ age-calculator-app-main/main.js
@@ -1,1 +1,20 @@
+// DOM Elements & global variables
+const yearsDisplayEl = document.getElementById('years-result');
+const monthsDisplayEl = document.getElementById('months-result');
+const daysDisplayEl = document.getElementById('days-result');
+
+let yearsResult = 38;
+let monthsResult = 3;
+let daysResult = 26;
+
+// Functions
+
+yearsDisplayEl.textContent = yearsResult;
+monthsDisplayEl.textContent = monthsResult;
+daysDisplayEl.textContent = daysResult;
+
 console.log(".");
+
+
+
+// Event listeners
