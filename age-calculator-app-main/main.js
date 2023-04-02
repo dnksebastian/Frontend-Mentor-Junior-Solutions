07b404563edbbc1c0349c3c5f756595d9bb34a77
dnksebastian <sebastian.dulniawka@gmail.com>
2023-04-02 15:49:37 +0200

    age calculator app

--- age-calculator-app-main/main.js
+++ age-calculator-app-main/main.js
@@ -1,11 +1,18 @@
 // DOM Elements & global variables
+
+const submitBtn = document.getElementById('submit-btn');
+const calculatorFormEl = document.getElementById('calc-form');
+
 const yearsDisplayEl = document.getElementById('years-result');
 const monthsDisplayEl = document.getElementById('months-result');
 const daysDisplayEl = document.getElementById('days-result');
 
-let yearsResult = 38;
-let monthsResult = 3;
-let daysResult = 26;
+// let yearsResult = 38;
+// let monthsResult = 3;
+// let daysResult = 26;
+let yearsResult = "--";
+let monthsResult = "--";
+let daysResult = "--";
 
 // Functions
 
@@ -13,8 +20,12 @@ yearsDisplayEl.textContent = yearsResult;
 monthsDisplayEl.textContent = monthsResult;
 daysDisplayEl.textContent = daysResult;
 
-console.log(".");
 
+const calculateResult = (e) => {
+    e.preventDefault();
+    console.log('Calculate');
+};
 
 
 // Event listeners
+calculatorFormEl.addEventListener('submit', calculateResult);
