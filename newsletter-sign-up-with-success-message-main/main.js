ee08336ba7482d491c5431678031237cc4831d42
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-27 00:03:14 +0200

    newsletter signup component

--- newsletter-sign-up-with-success-message-main/main.js
+++ newsletter-sign-up-with-success-message-main/main.js
@@ -1,13 +1,45 @@
 // DOM Elements
-const componentFormEl = document.getElementById('component-form');
+const componentFormEl = document.getElementById("component-form");
+const emailInputEl = document.getElementById("email");
+const errMsgEl = document.getElementById("err-msg");
 
 // Functions
 
 const submitForm = (e) => {
-    e.preventDefault()
-    console.log('submit form');
-}
+  e.preventDefault();
+  let isValid = validateInput();
+
+  if (isValid) {
+    console.log("submit form");
+  } else {
+    return;
+  }
+};
+
+const validateInput = (e) => {
+  if (emailInputEl.validity.valueMissing) {
+    errMsgEl.textContent = "Email is required";
+    return false;
+  } else if (emailInputEl.validity.typeMismatch) {
+    errMsgEl.textContent = "Valid email required";
+    return false;
+  } else if (emailInputEl.validity.valid) {
+    errMsgEl.textContent = "";
+    return true;
+  }
+};
 
 // Event listeners
 
-componentFormEl.addEventListener('submit', submitForm)
+componentFormEl.addEventListener("submit", submitForm);
+emailInputEl.addEventListener('input', validateInput);
+emailInputEl.addEventListener('blur', validateInput);
+
+
+
+emailInputEl.addEventListener('click', () => {
+    componentFormEl.classList.add('interacted')
+})
+componentFormEl.addEventListener('submit', () => {
+    componentFormEl.classList.add('interacted')
+})
