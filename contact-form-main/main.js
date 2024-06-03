7c042934859b42a802544b2c1d5444248bc24853
dnksebastian <sebastian.dulniawka@gmail.com>
2024-06-03 23:02:13 +0200

    contact form

--- contact-form-main/main.js
+++ contact-form-main/main.js
@@ -1,1 +1,110 @@
-console.log('.');
+// DOM Elements
+const formElement = document.getElementById('component-form');
+const inputWrappersEls = document.querySelectorAll('.input-wrapper');
+const formFields = Array.from(formElement.elements);
+const toastBoxEl = document.getElementById('toast-box');
+const toastTemplateEl = document.getElementById('toast-template');
+
+// Functions
+
+const changeTouched = (e) => {
+    const parentForm = e.target.closest('form');
+    const parentWrap = e.target.closest('.input-wrapper');
+
+    if (parentForm) {
+        parentForm.classList.add('touched');
+    }
+
+    if (parentWrap) {
+        parentWrap.classList.add('wrap-touched');
+    }
+};
+
+const addFocused = (e) => {
+    const parentWrap = e.target.closest('.input-wrapper');
+    if (parentWrap) {
+        parentWrap.classList.add('wrap-focused');
+    }
+};
+
+const removeFocused = (e) => {
+    const parentWrap = e.target.closest('.input-wrapper');
+    if (parentWrap) {
+        parentWrap.classList.remove('wrap-focused');
+    }
+};
+
+const validateInputs = () => {
+    formFields.forEach(el => {
+        const closestWrapper = el.closest('.input-wrapper');
+
+        if (!el.checkValidity()) {
+            if (closestWrapper) {
+                closestWrapper.classList.add('invalid');
+            }
+        } else {
+            if (closestWrapper) {
+                closestWrapper.classList.remove('invalid')
+            }
+        }
+    });
+}
+
+const displayToast = () => {
+    const toastElement = toastTemplateEl.content.cloneNode(true);
+    toastBoxEl.appendChild(toastElement);
+    setTimeout(() => {
+        toastBoxEl.replaceChildren();
+    }, 5000);
+};
+
+const displayInitialToast = () => {
+    const toastElement = toastTemplateEl.content.cloneNode(true);
+    toastElement.querySelector('h2').textContent = 'Information';
+    toastElement.querySelector('p').textContent = 'Form is reset with each page refresh!';
+
+    toastBoxEl.appendChild(toastElement);
+    setTimeout(() => {
+        toastBoxEl.replaceChildren();
+    }, 5000);
+};
+
+const submitForm = (e) => {
+    e.preventDefault();
+    
+    validateInputs(e)
+
+    const formIsValid = formElement.checkValidity();
+
+    if (formIsValid) {
+        displayToast()
+        formElement.reset()
+        formElement.classList.remove('touched');
+        formFields.forEach(el => {
+            el.classList.remove('wrap-touched');
+            el.classList.remove('wrap-focused');
+        });
+    } else {
+        console.log('There are invalid fields.');
+        return;
+    }
+};
+
+// Event listeners
+formElement.addEventListener('submit', submitForm);
+formElement.addEventListener('blur', validateInputs)
+formElement.addEventListener('input', validateInputs)
+
+formFields.forEach(el => {
+    el.addEventListener('focus', changeTouched)
+    el.addEventListener('focus', addFocused)
+});
+
+formFields.forEach(el => {
+    el.addEventListener('blur', removeFocused)
+});
+
+formElement.addEventListener('focus', changeTouched);
+
+displayInitialToast()
+formElement.reset()
