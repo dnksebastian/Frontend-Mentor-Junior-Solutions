e64aa99a9d6eca9df9b697899f57b5eaafb98774
dnksebastian <sebastian.dulniawka@gmail.com>
2024-07-01 19:28:52 +0200

    mortgage calculator

--- mortgage-repayment-calculator-main/main.js
+++ mortgage-repayment-calculator-main/main.js
@@ -1,1 +1,12 @@
-console.log('.');
+// DOM Elements
+const formElement = document.getElementById("calculator-form");
+
+// Functions
+const submitForm = (e) => {
+  e.preventDefault();
+
+  console.log("form sent");
+};
+
+// Event listeners
+formElement.addEventListener("submit", submitForm);
